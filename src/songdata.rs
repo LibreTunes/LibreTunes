a65c7993d55a3272744222f895ebfc4e0ea46732
Ethan Girouard <mr.egirouard@gmail.com>
2024-01-01 15:08:13 -0500

    Add SongData struct

---
+++ src/songdata.rs
@@ -0,0 +1,16 @@
+/// Holds information about a song
+#[derive(Debug)]
+pub struct SongData {
+	/// Song name
+	pub name: String,
+	/// Song artist
+	pub artist: String,
+	/// Song album
+	pub album: String,
+	/// Path to song file, relative to the root of the web server.
+	/// For example, `"/assets/audio/Song.mp3"`
+	pub song_path: String,
+	/// Path to song image, relative to the root of the web server.
+	/// For example, `"/assets/images/Song.jpg"`
+	pub image_path: String,
+}
