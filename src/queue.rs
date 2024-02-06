604e2c9c7fa91951b7f1de58a6e30d9778b21554
dannyzou18 <dzou3814@gmail.com>
2024-02-06 16:30:24 -0500

    created simple song component for queue

--- src/queue.rs
+++ src/queue.rs
@@ -3,6 +3,19 @@ use leptos::leptos_dom::*;
 use leptos::*;
 
 #[component]
+fn Song(song_image_path: String, song_title: String, song_artist: String) -> impl IntoView {
+	view!{
+		<div class="queue-song">
+			<img src={song_image_path} alt={song_title.clone()} />
+			<div class="queue-song-info">
+				<h3>{song_title}</h3>
+				<p>{song_artist}</p>
+			</div>
+		</div>
+	}
+}
+
+#[component]
 pub fn Queue(status: RwSignal<PlayStatus>) -> impl IntoView {
 
 	view!{
@@ -17,13 +30,7 @@ pub fn Queue(status: RwSignal<PlayStatus>) -> impl IntoView {
 					{
 						status.with(|status| status.queue.iter()
 							.map(|song| view! {
-								<div class="queue-song">
-									<img src={song.image_path.clone()} alt={song.name.clone()}/>
-									<div class="queue-song-info">
-										<h3>{song.name.clone()}</h3>
-										<p>{song.artist.clone()}</p>
-									</div>
-								</div>
+								<Song song_image_path=song.image_path.clone() song_title=song.name.clone() song_artist=song.artist.clone() />
 							})
 							.collect::<Vec<_>>())
 					}
