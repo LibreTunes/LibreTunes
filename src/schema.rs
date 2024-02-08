319958f26470720fb9f49e9aedceceb343f810cd
Ethan Girouard <mr.egirouard@gmail.com>
2024-02-08 18:38:36 -0500

    Create songs table

--- src/schema.rs
+++ src/schema.rs
@@ -23,6 +23,26 @@ diesel::table! {
 }
 
 diesel::table! {
+    song_artists (song_id, artist_id) {
+        song_id -> Int4,
+        artist_id -> Int4,
+    }
+}
+
+diesel::table! {
+    songs (id) {
+        id -> Int4,
+        title -> Varchar,
+        album_id -> Nullable<Int4>,
+        track -> Nullable<Int4>,
+        duration -> Int4,
+        release_date -> Nullable<Date>,
+        storage_path -> Varchar,
+        image_path -> Nullable<Varchar>,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         username -> Varchar,
@@ -34,10 +54,15 @@ diesel::table! {
 
 diesel::joinable!(album_artists -> albums (album_id));
 diesel::joinable!(album_artists -> artists (artist_id));
+diesel::joinable!(song_artists -> artists (artist_id));
+diesel::joinable!(song_artists -> songs (song_id));
+diesel::joinable!(songs -> albums (album_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
     album_artists,
     albums,
     artists,
+    song_artists,
+    songs,
     users,
 );
