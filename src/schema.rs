0924c954b800cdf27f9a773f1d07748e555afaf8
Ethan Girouard <mr.egirouard@gmail.com>
2024-02-08 18:37:55 -0500

    Create artists table

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,13 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    artists (id) {
+        id -> Int4,
+        name -> Varchar,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         username -> Varchar,
@@ -9,3 +16,8 @@ diesel::table! {
         created_at -> Timestamp,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    artists,
+    users,
+);
