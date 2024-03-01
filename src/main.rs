dcd08ff46e31a8b89be3f55193dc04357c9d912d
Ethan Girouard <mr.egirouard@gmail.com>
2024-03-01 15:03:23 -0500

    Remove favicon server

--- src/main.rs
+++ src/main.rs
@@ -47,18 +47,6 @@ async fn main() {
         .unwrap();
 }
 
-#[cfg(feature = "ssr")]
-#[actix_web::get("favicon.ico")]
-async fn favicon(
-    leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
-) -> actix_web::Result<actix_files::NamedFile> {
-    let leptos_options = leptos_options.into_inner();
-    let site_root = &leptos_options.site_root;
-    Ok(actix_files::NamedFile::open(format!(
-        "{site_root}/favicon.ico"
-    ))?)
-}
-
 #[cfg(not(any(feature = "ssr", feature = "csr")))]
 pub fn main() {
     // no client-side main function
