d8e7ffd96390349c899885d60f4cbc0c990b63ce
ecco257 <72117210+ecco257@users.noreply.github.com>
2024-03-15 16:53:12 -0400

    Separate Song component from queue styling to use for general case

--- src/song.rs
+++ src/song.rs
@@ -3,9 +3,9 @@ use leptos::*;
 #[component]
 pub fn Song(song_image_path: String, song_title: String, song_artist: String) -> impl IntoView {
 	view!{
-		<div class="queue-song">
+		<div class="song">
 			<img src={song_image_path} alt={song_title.clone()} />
-			<div class="queue-song-info">
+			<div class="song-info">
 				<h3>{song_title}</h3>
 				<p>{song_artist}</p>
 			</div>
