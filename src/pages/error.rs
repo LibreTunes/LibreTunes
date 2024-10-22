f1e177c7b074ee6b5367aa52d7e6303e0c0b88aa
Ethan Girouard <ethan@girouard.com>
2024-10-21 22:57:27 -0400

    Add page for displaying error

---
+++ src/pages/error.rs
@@ -0,0 +1,24 @@
+use leptos::*;
+use leptos_icons::*;
+use std::fmt::Display;
+
+#[component]
+pub fn ServerError<E: Display + 'static>(
+	#[prop(optional, into, default="An Error Occurred".into())]
+	title: TextProp,
+	#[prop(optional, into)]
+	message: TextProp,
+	#[prop(optional, into)]
+	error: Option<ServerFnError<E>>,
+) -> impl IntoView {
+	view! {
+		<div class="error-container home-component">
+			<div class="error-header">
+				<Icon icon=icondata::BiErrorSolid />
+				<h1>{title}</h1>
+			</div>
+			<p>{message}</p>
+			<p>{error.map(|error| format!("{}", error))}</p>
+		</div>
+	}
+}
