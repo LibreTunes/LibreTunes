833393cb3a6426121b3a6e222ac1dc3323750d05
Ethan Girouard <ethan@girouard.com>
2024-11-04 17:11:26 -0500

    Add generic Error component

--- src/components/error.rs
+++ src/components/error.rs
@@ -11,6 +11,27 @@ pub fn ServerError<E: Display + 'static>(
 	#[prop(optional, into)]
 	error: Option<ServerFnError<E>>,
 ) -> impl IntoView {
+	view!{
+		<div class="error-container">
+			<div class="error-header">
+				<Icon icon=icondata::BiErrorSolid />
+				<h1>{title}</h1>
+			</div>
+			<p>{message}</p>
+			<p>{error.map(|error| format!("{}", error))}</p>
+		</div>
+	}
+}
+
+#[component]
+pub fn Error<E: Display + 'static>(
+	#[prop(optional, into, default="An Error Occurred".into())]
+	title: TextProp,
+	#[prop(optional, into)]
+	message: TextProp,
+	#[prop(optional, into)]
+	error: Option<E>,
+) -> impl IntoView {
 	view! {
 		<div class="error-container">
 			<div class="error-header">
