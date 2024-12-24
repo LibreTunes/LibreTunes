782c9b94824062875dea7698ab128e231c5685be
Ethan Girouard <ethan@girouard.com>
2024-12-23 21:04:20 -0500

    Use correct use_params_map import path

--- src/pages/songpage.rs
+++ src/pages/songpage.rs
@@ -1,6 +1,6 @@
 use leptos::prelude::*;
 use leptos::either::*;
-use leptos_router::use_params_map;
+use leptos_router::hooks::use_params_map;
 use leptos_icons::*;
 use server_fn::error::NoCustomError;
 
