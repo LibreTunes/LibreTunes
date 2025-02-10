49455f5e037d05a4d408f7d8168e5069038ad2a8
Ethan Girouard <ethan@girouard.com>
2025-02-10 17:25:57 -0500

    Convert loading to use Tailwind styling

--- src/components/loading.rs
+++ src/components/loading.rs
@@ -3,8 +3,15 @@ use leptos::prelude::*;
 /// A loading indicator
 #[component]
 pub fn Loading() -> impl IntoView {
+	let dots_style = "h-2 w-2 bg-accent rounded-full animate-pulse";
+
 	view! {
-		<div class="loading"></div>
+		<div class="flex space-x-1 justify-center items-center bg-white my-2">
+			<span class="sr-only">"Loading..."</span>
+			<div class=dots_style style="animation-duration: 900ms; animation-delay: 0ms;" />
+			<div class=dots_style style="animation-duration: 900ms; animation-delay: 300ms"/>
+			<div class=dots_style style="animation-duration: 900ms; animation-delay: 600ms;" />
+		</div>
 	}
 }
 
