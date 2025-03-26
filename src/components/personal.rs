b735b25677e6822f5ac3b115d0331185a3046a7f
Ethan Girouard <ethan@girouard.com>
2025-03-26 17:09:59 -0400

    Use tailwindcss for Personal component
    Close login/out buttons when clicking away

--- src/components/personal.rs
+++ src/components/personal.rs
@@ -2,6 +2,9 @@ use leptos::leptos_dom::*;
 use leptos::prelude::*;
 use leptos_icons::*;
 use leptos::task::spawn_local;
+use leptos::html::Div;
+use leptos_use::on_click_outside_with_options;
+use leptos_use::OnClickOutsideOptions;
 use crate::api::auth::logout;
 use crate::util::state::GlobalState;
 
@@ -16,12 +19,16 @@ pub fn Personal() -> impl IntoView {
 
 #[component]
 pub fn Profile() -> impl IntoView {
-    let (dropdown_open, set_dropdown_open) = signal(false);
+    let dropdown_open = RwSignal::new(false);
 	let user = GlobalState::logged_in_user();
-    
-	let open_dropdown = move |_| {
-        set_dropdown_open.update(|value| *value = !*value);
-    };
+
+	let toggle_dropdown = move |_| dropdown_open.set(!dropdown_open.get());
+
+    let profile_photo = NodeRef::<Div>::new();
+    let dropdown = NodeRef::<Div>::new();
+    let _ = on_click_outside_with_options(dropdown, move |_| dropdown_open.set(false),
+        OnClickOutsideOptions::default().ignore(profile_photo)
+    );
 
     let user_profile_picture = move || {
         user.get().and_then(|user| {
@@ -35,8 +42,8 @@ pub fn Profile() -> impl IntoView {
     };
 
     view! {
-        <div class="profile-container">
-            <div class="profile-name">
+        <div class="flex w-50 relative">
+            <div class="text-lg self-center">
                 <Suspense
                     fallback=|| view!{
                         <h1>Not Logged In</h1>
@@ -50,43 +57,45 @@ pub fn Profile() -> impl IntoView {
 					</Show>
                 </Suspense>
             </div>
-            <div class="profile-icon" on:click=open_dropdown>
+            <div class="self-center hover:scale-105 transition-transform cursor-pointer ml-auto"
+                on:click=toggle_dropdown node_ref=profile_photo>
 				<Suspense fallback=|| view! { <Icon icon={icondata::CgProfile} width="45" height="45"/> }>
 					<Show 
 						when=move || user.get().map(|user| user.is_some()).unwrap_or(false)
 						fallback=|| view! { <Icon icon={icondata::CgProfile} width="45" height="45"/> }
 					>
-						<object class="profile-image" data={user_profile_picture} type="image/webp">
-							<Icon icon={icondata::CgProfile} width="45" height="45" {..} class="profile-image" />
+						<object class="w-11 h-11 rounded-full pointer-events-none"
+                            data={user_profile_picture} type="image/webp">
+							<Icon icon={icondata::CgProfile} width="45" height="45" {..} />
 						</object>
 					</Show>
 				</Suspense>
             </div>
-            <div class="dropdown-container" style={move || if dropdown_open() {"display: flex"} else {"display: none"}}>
-				<Suspense
-					fallback=|| view!{
-						<DropDownNotLoggedIn />
-					}>
-					<Show
-						when=move || user.get().map(|user| user.is_some()).unwrap_or(false)
-						fallback=|| view!{
-							<DropDownNotLoggedIn />
-						}>
-						<DropDownLoggedIn />
-					</Show>
-				</Suspense>
-            </div>
+            <Show when=dropdown_open >
+                <div class="absolute bg-bg-light rounded-lg border-2 border-neutral-700 top-12
+                    right-3 p-1 text-right" node_ref=dropdown>
+                    <Suspense
+                        fallback=|| view!{
+                            <DropDownNotLoggedIn />
+                        }>
+                        <Show
+                            when=move || user.get().map(|user| user.is_some()).unwrap_or(false)
+                            fallback=|| view!{
+                                <DropDownNotLoggedIn />
+                            }>
+                            <DropDownLoggedIn />
+                        </Show>
+                    </Suspense>
+                </div>
+            </Show>
         </div>
     }
 }
 #[component]
 pub fn DropDownNotLoggedIn() -> impl IntoView {
     view! {
-        <div class="dropdown-logged">
-            <h1>Not Logged In</h1>
-            <a href="/login"><button class="auth-button">Log In</button></a>
-            <a href="/signup"><button class="auth-button">Sign Up</button></a>
-        </div>
+        <a href="/login"><button class="auth-button">"Log In"</button></a><br/>
+        <a href="/signup"><button class="auth-button">"Sign Up"</button></a>
     }
 }
 #[component]
@@ -106,9 +115,6 @@ pub fn DropDownLoggedIn() -> impl IntoView {
     };
 
     view! {
-        <div class="dropdown-logged">
-            <h1>"Logged In"</h1>
-            <button on:click=logout class="auth-button">Log Out</button>
-        </div>
+        <button on:click=logout class="auth-button">"Log Out"</button>
     }
 }
